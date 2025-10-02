8a4b43983094c5277b360af08233baf65acdd9ed
Ruchit Sheta <ruchit.sheta@zeuslearning.com>
2025-10-02 19:25:07 +0530

    RED-GREEN-REFACTOR: Handle any amount of numbers

--- src/StringCalculator.js
+++ src/StringCalculator.js
@@ -1,15 +1,26 @@
 class StringCalculator {
   add(numbers) {
-    if(numbers === '') {
+    if(this._isEmpty(numbers)) {
       return 0;
     }
 
-    if(numbers.includes(',')) {
-      const parts = numbers.split(',');
-      return parts.reduce((sum, num)=> sum + parseInt(num), 0);
-    }
+    const numbersArray = this._parseNumbers(numbers);
+    return this._calculateSum(numbersArray);
+  }
 
-    return parseInt(numbers); 
+  _isEmpty(numbers) {
+    return numbers === '';
+  }
+
+  _parseNumbers(numbers) {
+    if (numbers.includes(',')) {
+      return numbers.split(',');
+    }
+    return [numbers];
+  }
+
+  _calculateSum(numbersArray) {
+    return numbersArray.reduce((sum, num) => sum + parseInt(num), 0);
   }
 }
 
